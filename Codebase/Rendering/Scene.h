8e84ff7776181cdc3d9db820ef988a1e113da8ac
Shaun Heald <s.s.heald@newcastle.ac.uk>
2016-02-04 18:55:30 +0000

    bug fixes

--- Codebase/Rendering/Scene.h
+++ Codebase/Rendering/Scene.h
@@ -9,16 +9,16 @@ public:
 	~Scene();
 
 	//return types may be changed
-	GameObject* getOpaqueObjects(); 
-	GameObject* getTransparentObjects();
-	GameObject* getAllObjects();
+	//GameObject* getOpaqueObjects(); 
+	//GameObject* getTransparentObjects();
+	//GameObject* getAllObjects();
 
 	Camera* getCamera(){ return cam; }
 
 	int renderFlags; //triggers for  graphical effects such as post processing and any other info required by the renderer
 
 private:
-	std::vector<GameObject> objects;
+	//std::vector<GameObject> objects;
 	Camera* cam;	
 };
 
